ad8cabd25bde93e422341e4428e452b68234404c
jwhil <j.g.whillock@gmail.com>
2024-03-13 11:39:41 +0000

    ticket 4 complete

--- users/users.test.js
+++ users/users.test.js
@@ -1,3 +1,27 @@
-import { test, expect } from "vitest";
+import { test, expect, describe, it } from "vitest";
+import request from "supertest";
+import app from "../app.js";
 
 test("GET/api/health works", () => {});
+
+describe("GET /api/health", function () {
+  it("responds with json", async function () {
+    const response = await request(app).get("/api/health");
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+    expect(response.body.success).toBe(true);
+    expect(response.body.payload).toEqual("API is running correctly");
+  });
+});
+
+// describe("GET /api/health works", function () {
+//     it("responds with json", async function () {
+//       const response = await request(app).get("/api/health");
+
+//       expect(response.headers["content-type"]).toMatch(/json/);
+//       expect(response.status).toEqual(200);
+
+//       expect(response.body.success).toBe(true);
+//       expect(response.body.payload).toEqual("API is running correctly");
+//     });
+// });
